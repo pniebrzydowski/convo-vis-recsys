1e3d0939bc9c67eb1d2f08a49930d187276bf338
Patrick Niebrzydowski <patrick.niebrzydowski@students.fh-hagenberg.at>
2017-01-07 00:48:36 +0100

    Initial setup for webpack, babel, and react

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './src/client/app/index.js',
+  output: {
+    filename: 'app.js',
+    path: './src/client/public'
+  },
+  module: {
+	loaders: [
+		{ 
+			test: /\.js$/,
+			exclude: /node_modules/, 
+			loader: 'babel-loader'
+		}
+	],
+  }
+}

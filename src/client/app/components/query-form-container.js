076fb2bd9c85440cf8de07aee1e159739426dc4b
Patrick Niebrzydowski <patrick.niebrzydowski@students.fh-hagenberg.at>
2017-01-08 02:47:10 +0100

    create word cloud out of tips

--- src/client/app/components/query-form-container.js
+++ src/client/app/components/query-form-container.js
@@ -8,7 +8,7 @@ class QueryFormContainer extends React.Component {
 		this.state = {
       query: null,
       loc: null,
-			venue: null
+			tips: []
 		}
 		
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -32,31 +32,574 @@ class QueryFormContainer extends React.Component {
 		  "near": self.state.loc,
 		  "query": self.state.query
 		};
+    self.tips = [];
 		
 		self.foursquare.venues.explore(params)
 		  .then(function(data) {
 				console.log(data);
-				self.findSingleVenue(data.response.groups[0].items[0].venue.id);
+				self.getVenueTips(data.response.groups[0].items);
 			})
 		  .catch(function(err){
         console.log(err);
       });
 	}
 	
-	findSingleVenue(id) {
+	getVenueTips(venues) {
 		var self = this;
 		
-		let params = {venue_id: id};
-
-		self.foursquare.venues.getVenue(params)
-			.then(function(data) {
-				console.log(data);
-				self.venue = data.response.venue;
-			})
-			.catch(function(err) {
-				console.log(err);
-			})
+    let venueCt = 0;    
+    for(let i=0; i<venues.length; i++) {
+      let params = {venue_id: venues[i].venue.id, aspect_name: "tips"};
+      self.foursquare.venues.getVenueAspect(params)
+        .then(function(data) {
+          let tips = data.response.tips.items;
+          let words = [];
+          for(let j=0; j<tips.length; j++) {
+            let sentence = tips[j].text.replace(/[^\w\s]|_/g, "").replace(/\d+/g, '').replace(/\s+/g, " ").toLowerCase();
+            sentence = self.removeStopWords(sentence);
+            words = words.concat(sentence.split(/\s*\b\s*/));
+          }
+          self.state.tips = words;
+          venueCt++;
+          if(venueCt === venues.length) {
+            self.processTips();
+          }
+        })
+        .catch(function(err) {
+          console.log(err);
+        })
+    };
 	}
+  
+  sortByFrequency(array) {
+      var frequency = {};
+
+      array.forEach(function(value) { frequency[value] = 0; });
+
+      var uniques = array.filter(function(value) {
+          return ++frequency[value] == 1;
+      });
+
+      return uniques.sort(function(a, b) {
+          return frequency[b] - frequency[a];
+      });
+  }
+  
+  removeStopWords(string) {
+    var x;
+    var y;
+    var word;
+    var stop_word;
+    var regex_str;
+    var regex;
+    var cleansed_string = string.valueOf();
+    var stop_words = new Array(
+        'a',
+        'about',
+        'above',
+        'across',
+        'after',
+        'again',
+        'against',
+        'all',
+        'almost',
+        'alone',
+        'along',
+        'already',
+        'also',
+        'although',
+        'always',
+        'among',
+        'an',
+        'and',
+        'another',
+        'any',
+        'anybody',
+        'anyone',
+        'anything',
+        'anywhere',
+        'are',
+        'area',
+        'areas',
+        'around',
+        'as',
+        'ask',
+        'asked',
+        'asking',
+        'asks',
+        'at',
+        'away',
+        'b',
+        'back',
+        'backed',
+        'backing',
+        'backs',
+        'be',
+        'became',
+        'because',
+        'become',
+        'becomes',
+        'been',
+        'before',
+        'began',
+        'behind',
+        'being',
+        'beings',
+        'best',
+        'better',
+        'between',
+        'big',
+        'both',
+        'but',
+        'by',
+        'c',
+        'came',
+        'can',
+        'cannot',
+        'case',
+        'cases',
+        'certain',
+        'certainly',
+        'clear',
+        'clearly',
+        'come',
+        'could',
+        'd',
+        'did',
+        'differ',
+        'different',
+        'differently',
+        'do',
+        'does',
+        'done',
+        'down',
+        'down',
+        'downed',
+        'downing',
+        'downs',
+        'during',
+        'e',
+        'each',
+        'early',
+        'either',
+        'end',
+        'ended',
+        'ending',
+        'ends',
+        'enough',
+        'even',
+        'evenly',
+        'ever',
+        'every',
+        'everybody',
+        'everyone',
+        'everything',
+        'everywhere',
+        'f',
+        'face',
+        'faces',
+        'fact',
+        'facts',
+        'far',
+        'felt',
+        'few',
+        'find',
+        'finds',
+        'first',
+        'for',
+        'four',
+        'from',
+        'full',
+        'fully',
+        'further',
+        'furthered',
+        'furthering',
+        'furthers',
+        'g',
+        'gave',
+        'general',
+        'generally',
+        'get',
+        'gets',
+        'give',
+        'given',
+        'gives',
+        'go',
+        'going',
+        'good',
+        'goods',
+        'got',
+        'great',
+        'greater',
+        'greatest',
+        'group',
+        'grouped',
+        'grouping',
+        'groups',
+        'h',
+        'had',
+        'has',
+        'have',
+        'having',
+        'he',
+        'her',
+        'here',
+        'herself',
+        'high',
+        'high',
+        'high',
+        'higher',
+        'highest',
+        'him',
+        'himself',
+        'his',
+        'how',
+        'however',
+        'i',
+        'if',
+        'important',
+        'in',
+        'interest',
+        'interested',
+        'interesting',
+        'interests',
+        'into',
+        'is',
+        'it',
+        'its',
+        'itself',
+        'j',
+        'just',
+        'k',
+        'keep',
+        'keeps',
+        'kind',
+        'knew',
+        'know',
+        'known',
+        'knows',
+        'l',
+        'large',
+        'largely',
+        'last',
+        'later',
+        'latest',
+        'least',
+        'less',
+        'let',
+        'lets',
+        'like',
+        'likely',
+        'long',
+        'longer',
+        'longest',
+        'm',
+        'made',
+        'make',
+        'making',
+        'man',
+        'many',
+        'may',
+        'me',
+        'member',
+        'members',
+        'men',
+        'might',
+        'more',
+        'most',
+        'mostly',
+        'mr',
+        'mrs',
+        'much',
+        'must',
+        'my',
+        'myself',
+        'n',
+        'necessary',
+        'need',
+        'needed',
+        'needing',
+        'needs',
+        'never',
+        'new',
+        'new',
+        'newer',
+        'newest',
+        'next',
+        'no',
+        'nobody',
+        'non',
+        'noone',
+        'not',
+        'nothing',
+        'now',
+        'nowhere',
+        'number',
+        'numbers',
+        'o',
+        'of',
+        'off',
+        'often',
+        'old',
+        'older',
+        'oldest',
+        'on',
+        'once',
+        'one',
+        'only',
+        'open',
+        'opened',
+        'opening',
+        'opens',
+        'or',
+        'order',
+        'ordered',
+        'ordering',
+        'orders',
+        'other',
+        'others',
+        'our',
+        'out',
+        'over',
+        'p',
+        'part',
+        'parted',
+        'parting',
+        'parts',
+        'per',
+        'perhaps',
+        'place',
+        'places',
+        'point',
+        'pointed',
+        'pointing',
+        'points',
+        'possible',
+        'present',
+        'presented',
+        'presenting',
+        'presents',
+        'problem',
+        'problems',
+        'put',
+        'puts',
+        'q',
+        'quite',
+        'r',
+        'rather',
+        'really',
+        'right',
+        'right',
+        'room',
+        'rooms',
+        's',
+        'said',
+        'same',
+        'saw',
+        'say',
+        'says',
+        'second',
+        'seconds',
+        'see',
+        'seem',
+        'seemed',
+        'seeming',
+        'seems',
+        'sees',
+        'several',
+        'shall',
+        'she',
+        'should',
+        'show',
+        'showed',
+        'showing',
+        'shows',
+        'side',
+        'sides',
+        'since',
+        'small',
+        'smaller',
+        'smallest',
+        'so',
+        'some',
+        'somebody',
+        'someone',
+        'something',
+        'somewhere',
+        'state',
+        'states',
+        'still',
+        'still',
+        'such',
+        'sure',
+        't',
+        'take',
+        'taken',
+        'than',
+        'that',
+        'the',
+        'their',
+        'them',
+        'then',
+        'there',
+        'therefore',
+        'these',
+        'they',
+        'thing',
+        'things',
+        'think',
+        'thinks',
+        'this',
+        'those',
+        'though',
+        'thought',
+        'thoughts',
+        'three',
+        'through',
+        'thus',
+        'to',
+        'today',
+        'together',
+        'too',
+        'took',
+        'toward',
+        'turn',
+        'turned',
+        'turning',
+        'turns',
+        'two',
+        'u',
+        'under',
+        'until',
+        'up',
+        'upon',
+        'us',
+        'use',
+        'used',
+        'uses',
+        'v',
+        'very',
+        'w',
+        'want',
+        'wanted',
+        'wanting',
+        'wants',
+        'was',
+        'way',
+        'ways',
+        'we',
+        'well',
+        'wells',
+        'went',
+        'were',
+        'what',
+        'when',
+        'where',
+        'whether',
+        'which',
+        'while',
+        'who',
+        'whole',
+        'whose',
+        'why',
+        'will',
+        'with',
+        'within',
+        'without',
+        'work',
+        'worked',
+        'working',
+        'works',
+        'would',
+        'x',
+        'y',
+        'year',
+        'years',
+        'yet',
+        'you',
+        'young',
+        'younger',
+        'youngest',
+        'your',
+        'yours',
+        'z'
+    )
+         
+    // Split out all the individual words in the phrase
+    var words = cleansed_string.match(/[^\s]+|\s+[^\s+]$/g)
+ 
+    // Review all the words
+    for(x=0; x < words.length; x++) {
+        // For each word, check all the stop words
+        for(y=0; y < stop_words.length; y++) {
+            // Get the current word
+            word = words[x].replace(/\s+|[^a-z]+/ig, "");   // Trim the word and remove non-alpha
+             
+            // Get the stop word
+            stop_word = stop_words[y];
+             
+            // If the word matches the stop word, remove it from the keywords
+            if(word.toLowerCase() == stop_word) {
+                // Build the regex
+                regex_str = "^\\s*"+stop_word+"\\s*$";      // Only word
+                regex_str += "|^\\s*"+stop_word+"\\s+";     // First word
+                regex_str += "|\\s+"+stop_word+"\\s*$";     // Last word
+                regex_str += "|\\s+"+stop_word+"\\s+";      // Word somewhere in the middle
+                regex = new RegExp(regex_str, "ig");
+             
+                // Remove the word from the keywords
+                cleansed_string = cleansed_string.replace(regex, " ");
+            }
+        }
+    }
+    return cleansed_string.replace(/^\s+|\s+$/g, "");
+  }
+  
+  processTips() {
+    console.log(this.state.tips);
+    var d3 = require("d3");
+    var cloud = require("d3-cloud");
+    
+    var freqArray = this.sortByFrequency(this.state.tips);
+    console.log(freqArray);
+
+    var fill = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var layout = cloud()
+        .size([500, 500])
+        .words(freqArray
+          .map(function(d) {
+            return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+          }))
+        .padding(5)
+        .rotate(function() { return ~~(Math.random() * 1) * 90; })
+        .font("Impact")
+        .fontSize(function(d) { return d.size; })
+        .on("end", draw);
+    
+    layout.start();
+        
+    function draw(words) {
+      console.log(words);
+      d3.select("body").append("svg")
+          .attr("width", layout.size()[0])
+          .attr("height", layout.size()[1])
+          .append("g")
+          .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+          .selectAll("text")
+          .data(words)
+          .enter().append("text")
+          .style("font-size", function(d) { return d.size + "px"; })
+          .style("font-family", "Impact")
+          .style("fill", function(d, i) { return fill(i); })
+          .attr("text-anchor", "middle")
+          .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+          })
+          .text(function(d) { return d.text; });
+    }
+    
+  }
 	
 	handleQueryChange(event) {
 		this.setState({query: event.target.value});

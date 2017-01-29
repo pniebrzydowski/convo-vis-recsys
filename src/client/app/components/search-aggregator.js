bd1487ff2699ddd92a8f9c2a0cfaebeccb3b315a
Patrick Niebrzydowski <patrick.niebrzydowski@students.fh-hagenberg.at>
2017-01-29 23:50:50 +0100

    move logic to aggregator component, add slider component

--- src/client/app/components/search-aggregator.js
+++ src/client/app/components/search-aggregator.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import SingleInput from './single-input.js';
 import ValueList from './value-list.js';
 import StackedBarChart from './stacked-bar-chart.js';
+import SliderWeighted from './slider-weighted.js';
 import SearchFoursquare from './search-foursquare.js';
 
 class SearchAggregator extends React.Component {
@@ -9,24 +9,54 @@ class SearchAggregator extends React.Component {
 		super(props);
     
     this.state = {
-			queryValues: [],
-			lists: []
+			chartData: [],
+			chartSeries: [],
+			queryNames: [],
+			queryValues: []
 		}
-	
+
+		this.queries = [];
+		this.resultItems = [];
+		
 		this.handleQueryAdd = this.handleQueryAdd.bind(this);
     this.handleQueryRemove = this.handleQueryRemove.bind(this);
+		this.handleWeightChange = this.handleWeightChange.bind(this);
   }
 		
 	handleQueryAdd(results, query) {
 		var self = this;
+    		    
+    for(let i=0; i<results.length; i++) {
+      let found = false;
+      let dataObj = {
+        id: results[i].venue.id,
+        name: results[i].venue.name,
+        queryRanks: {}
+      };
+      
+      for(let j=0; j<self.resultItems.length; j++) {
+        if(self.resultItems[j].id === results[i].venue.id) {
+          dataObj = self.resultItems[j];
+          found = true;
+          break;
+        }
+      }
+      
+      dataObj.queryRanks[query] = (results.length - i)/(results.length);
+
+      if(!found) {
+        self.resultItems.push(dataObj);
+      }
+    }
 		
-		self.state.lists.push({
-			query: query,
-			list: results
+		self.queries.push({
+			name: query,
+			weight: -1,
+			items: results
 		});
-		self.state.queryValues.push(query);
-		self.setState(self.state);
-		console.log(self.state);
+		
+		self.adjustQueryWeighting();
+		self.drawChart();
 	}
   	
   handleQueryRemove(event) {
@@ -40,11 +70,119 @@ class SearchAggregator extends React.Component {
       }
     }
     if(idx > -1) {
-			self.state.queryValues.splice(idx, 1);
-			self.state.lists.splice(idx, 1);
-    }
+			let newLists = self.state.lists.splice(idx, 1);
+			let newVals = self.state.queryValues.splice(idx, 1);
+    }		
+  }
+	
+	handleWeightChange(values) {
+		var self = this;
 		
-		self.setState(self.state);
+		
+		for(let i=0; i<self.queries.length; i++) {
+			self.queries[i].weight = (i==0 ? values[i] : (values[i] - values[i-1]));
+		}
+		
+		self.drawChart();
+	}
+		
+	adjustQueryWeighting() {
+		var self = this;
+		
+		let wts = [];
+		let eqWt = 600 / self.queries.length;
+		let totalWt = 0;
+		
+		for(let i=0; i<self.queries.length; i++) {
+			self.queries[i].weight = eqWt;
+
+			/*
+			let query = self.queries[i];
+			
+			for(let j=0; j<self.state.queryWeights.length; j++) {
+				let oldWt = -1;
+				if(self.queryWeights[j].name === query) {
+					oldWt = self.queryWeights[j].weight;
+				}
+			}
+
+			
+			if(oldWt < 0) {
+				self.queryWeights.push({
+					field: query,
+					name: query,
+					weight: 600 / self.queries.length
+				})
+			} else {
+				self.queryWeights[query] = newWt;
+			}*/
+		}
+	}
+	
+	getTopVenues() {
+    var self = this;
+    
+		let allVenues = self.resultItems.slice();
+		
+		for(let i=0; i<allVenues.length; i++) {
+			let score = 0;
+			let ranks = allVenues[i].queryRanks;
+			
+			for(let j=0; j<self.queries.length; j++) {
+				let query = self.queries[j].name;
+				if(!ranks[query]) {
+					allVenues[i][query] = 0;
+				} else {
+					let weightedScore = self.queries[j].weight / 6 * ranks[query];
+					allVenues[i][query] = weightedScore;
+					score += weightedScore;
+				}
+			}
+			
+			allVenues[i].totalScore = score;
+		}
+    allVenues.sort(function(a,b) {
+      return b.totalScore - a.totalScore;
+    });
+    
+    return allVenues.slice(0,10).reverse();
+  }
+	
+	getQueries() {
+		var self = this;
+		
+		let queries = {
+			names: [],
+			weights: [],
+			seriesArray: []
+		};
+		
+		let wt = 0;
+		for(let i=0; i<self.queries.length; i++) {
+			wt += self.queries[i].weight;
+			queries.names.push(self.queries[i].name);
+			queries.weights.push(wt);
+			queries.seriesArray.push({
+        field: self.queries[i].name,
+        name: self.queries[i].name
+      });
+		}
+		
+		return queries;
+	}
+	
+	drawChart() {
+    var self = this;
+    
+		let qInfo = self.getQueries();
+		let newState = {
+      chartSeries: qInfo.seriesArray,
+      chartData: self.getTopVenues(),
+			queryNames: qInfo.names,
+			queryValues: qInfo.weights
+    };
+		
+    self.setState(newState);
   }
 		
 	render() {
@@ -55,14 +193,19 @@ class SearchAggregator extends React.Component {
 				/>
         
 				<ValueList
-          values={this.state.queryValues}
+          values={this.state.queryNames}
 					removeFunc={this.handleQueryRemove}
         />
 				
-				<StackedBarChart
-					lists={this.state.lists}
-					queries={this.state.queryValues}
+				<SliderWeighted
+					values={this.state.queryValues}
+					handleWeightChange={this.handleWeightChange}
 				/>
+				
+        <StackedBarChart
+          chartData = {this.state.chartData}
+          chartSeries = {this.state.chartSeries}
+        />
       </div>
  		);
 	}
--- src/client/app/components/stacked-bar-chart.js
+++ src/client/app/components/stacked-bar-chart.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import * as d3 from 'd3';
 import {BarStackHorizontalChart} from 'react-d3-basic';
-import ReactSlider from 'react-slider';
 
 class StackedBarChart extends React.Component {
 	constructor(props) {
 		super(props);
     
-    this.state = {
-      chartSeries: [],
-      chartData: [],
-			sliderStepSize: 600,
-			weightValues: []
-		}
     this.WIDTH = 800;
     this.HEIGHT = 500;
     this.xDomain = [0,100];
     this.xTickFormat = d3.format(".3");
     this.yScale = 'ordinal';
     this.MAX_RANK = 30;
-
-    this.venues = [];
-		
-    this.handleWeightChange = this.handleWeightChange.bind(this);
   }
   
   x(d) {
@@ -30,175 +19,26 @@ class StackedBarChart extends React.Component {
   }
   y(d) {
     return d.name;
-  }
-	
-	componentWillReceiveProps(nextProps) {
-		if(this.props.queries.length <= this.state.weightValues.length) return;
-		var self = this;
-		
-		let newList = nextProps.lists[nextProps.lists.length - 1];
-		self.addNewList(newList.list, newList.query);
-		self.drawChart();
-	}
-		
-  addNewList(venues, newQuery) {
-		var self = this;
-    		    
-    for(let i=0; i<venues.length; i++) {
-      let found = false;
-      let dataObj = {
-        id: venues[i].venue.id,
-        name: venues[i].venue.name,
-        queryRanks: {}
-      };
-      
-      for(let j=0; j<self.venues.length; j++) {
-        if(self.venues[j].id === venues[i].venue.id) {
-          dataObj = self.venues[j];
-          found = true;
-          break;
-        }
-      }
-      
-      dataObj.queryRanks[newQuery] = self.MAX_RANK - i;
-
-      if(!found) {
-        self.venues.push(dataObj);
-      }
-    }
-	}
-		
-	adjustQueryWeighting() {
-		var self = this;
-		
-		let wts = [];
-		let eqWt = 600 / self.props.queries.length;
-		let totalWt = 0;
-		
-		for(let i=0; i<self.props.queries.length; i++) {
-			totalWt += eqWt;
-			wts.push(totalWt);
-
-			/*
-			let query = self.props.queries[i];
-			
-			for(let j=0; j<self.queryWeights.length; j++) {
-				let oldWt = -1;
-				if(self.queryWeights[j].name === query) {
-					oldWt = self.queryWeights[j].weight;
-				}
-			}
-
-			
-			if(oldWt < 0) {
-				self.queryWeights.push({
-					field: query,
-					name: query,
-					weight: 600 / self.props.queries.length
-				})
-			} else {
-				self.queryWeights[query] = newWt;
-			}*/
-		}
-		
-		return wts;
-	}
-	
-  drawChart() {
-    var self = this;
-    
-		var sliderValues = self.adjustQueryWeighting();
-		var seriesArray = self.getSeriesArray();
-		var topVenues = self.getTopVenues(sliderValues);
-		
-    self.setState({
-      chartSeries: seriesArray,
-      chartData: topVenues,
-			sliderStepSize: 60 / self.props.queries.length,
-			weightValues: sliderValues
-    });
-		
-		console.log(topVenues);
-    console.log(sliderValues);
-  }
-    
-  getSeriesArray() {
-    var self = this;
-    
-    let seriesArray = [];
-    for(let i=0; i<self.props.queries.length; i++) {
-      seriesArray.push({
-        field: self.props.queries[i],
-        name: self.props.queries[i]
-      });
-    }
-    return seriesArray;
-  }
-	  
-  getTopVenues(weights) {
-    var self = this;
-    
-		let allVenues = self.venues.slice();
-		
-		for(let i=0; i<allVenues.length; i++) {
-			let score = 0;
-			let ranks = allVenues[i].queryRanks;
-			
-			for(let j=0; j<self.props.queries.length; j++) {
-				let query = self.props.queries[j];
-				if(!ranks[query]) {
-					allVenues[i][query] = 0;
-				} else {
-					let wt = weights[j] - (j===0 ? 0 : weights[j-1]);
-					let weightedScore = wt / 6 * ranks[query] / 30;
-					allVenues[i][query] = weightedScore;
-					score += weightedScore;
-				}
-			}
-			
-			allVenues[i].totalScore = score;
-		}
-    allVenues.sort(function(a,b) {
-      return b.totalScore - a.totalScore;
-    });
-    
-    return allVenues.slice(0,10).reverse();
-  }
-	
-  handleWeightChange(values) {
-		var self = this;
-		self.setState({weightValues: values});
-		console.log(self.state);
-		//self.drawChart();
-  }
+  }    
 		
 	render() {
+		if(this.props.chartData.length === 0) {
+			return null;
+		}
+		
 		return (
-      <div>
-				<ReactSlider
-					withBars
-					min={this.state.sliderStepSize*2}
-					max={600}
-					minDistance={this.state.sliderStepSize*2}
-					step={this.state.sliderStepSize}
-					value={this.state.weightValues}
-					onAfterChange={this.handleWeightChange}
-					pearling={true}
-				/>
-				
-        <BarStackHorizontalChart
-          data= {this.state.chartData}
-          width= {this.WIDTH}
-          height= {this.HEIGHT}
-          chartSeries = {this.state.chartSeries}
-          x= {this.x}
-          xDomain= {this.xDomain}
-          xTickFormat= {this.xTickFormat}
-          y= {this.y}
-          yScale = {this.yScale}
-          horizontal= {true}
-        />
-      </div>
+			<BarStackHorizontalChart
+				data= {this.props.chartData}
+				width= {this.WIDTH}
+				height= {this.HEIGHT}
+				chartSeries = {this.props.chartSeries}
+				x= {this.x}
+				xDomain= {this.xDomain}
+				xTickFormat= {this.xTickFormat}
+				y= {this.y}
+				yScale = {this.yScale}
+				horizontal= {true}
+			/>
  		);
 	}
 }

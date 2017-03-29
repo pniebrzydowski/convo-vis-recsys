9f8444a106b14bdec13b35deade75ebdfadff9bd
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-03-29 19:59:03 +0200

    fix slider weight adjustments

--- src/client/app/components/slider-multi.js
+++ src/client/app/components/slider-multi.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import ReactSlider from 'react-slider';
 
-const SliderMulti = (props) => (
-	<div className="weight-sliders">
-		{props.values.map(function(val) {
-			return (
-				<ReactSlider
-					key={val.name}
-					min={1}
-					max={10}
-					value={val.weight}
-					onAfterChange={props.handleWeightChange}
-					pearling={true}
-				/>
-			);
-		})}
-	</div>
-);
+class SliderMulti extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		var self = this;
+
+		if (this.props.values.length <= 1) {
+			return null;
+		}
+
+		var weightChange = function weightChange(value) {
+			var name = this.name;
+			var vals = [];
+
+			for(var i=0; i<self.props.values.length; i++) {
+				if(self.props.values[i].name === name) vals.push(value);
+				else vals.push(self.props.values[i].weight);
+			}
+			self.props.handleWeightChange(vals);
+		};
+
+		return (
+			<div className="weight-sliders">
+				{this.props.values.map(function (val) {
+					return (
+						<ReactSlider
+							key={val.name}
+							name={val.name}
+							min={1}
+							max={10}
+							value={val.weight}
+							onAfterChange={weightChange}
+						/>
+					);
+				})}
+			</div>
+		);
+	}
+}
 
 export default SliderMulti;

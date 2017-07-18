efd9a2e7367ec9de2bc12f378ef7d492b8aeb6fb
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-07-18 17:23:14 +0200

    update server config

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'src/client/')));
+
+app.listen(8080);
